3d7823c3e7a4117f1c5852f59564ed0130870835
IT21068478 <99879515+IT21068478@users.noreply.github.com>
2022-03-02 10:40:51 +0000

    Tute 3

--- exercise04.cpp
+++ exercise04.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/*#include <iostream>
 using namespace std;
 
 void print(int len, int wth);
@@ -16,6 +16,34 @@ int main() {
 void print(int len, int wth) {
    cout << "Length : " << len 
         << ", Width  : " << wth << endl;
-}
+}*/
 
 // Implement the Input Function here
+
+
+#include <iostream>
+using namespace std;
+void print(int len, int wth);
+void input(int &length, int &width);
+// Do not change the main() function
+int main() {
+    int length = 10, width = 5;
+    
+    input(length, width);
+    
+    print(length, width);
+    
+    return 0;
+}
+// Do not change the print() function
+void print(int len, int wth) {
+    cout << "Length : " << len << ", Width : " << wth << endl;
+}
+
+void input(int &length, int &width){
+	cout << "Enter length :";
+	cin >> &length;
+	
+	cout << "Enter width :";
+	cin >> &width;
+}
